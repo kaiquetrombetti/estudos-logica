f1e7c2e7a6ff6a7c58fc231bbce0730110002d67
kaiquetrombetti <darkkaique1912@gmail.com>
2024-06-16 20:38:10 -0300

    Subindo arquivos da aula 3

---
+++ Aula2/ex3.php
@@ -0,0 +1,5 @@
+$fahrenheit = readline ("insira a temperatura em Fahrenheit"); 
+if($fahrenheit) { 
+    $celcius = ($fahrenheit - 32) * 5 / 9; 
+    echo "A temperatura em Celcius é: $celcius"; 
+}
